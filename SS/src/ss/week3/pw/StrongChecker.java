9108b24b8d2753c642660e825a0868f54ec69a1d
Sander <s.p.r.bloemen@student.utwente.nl>
2015-11-30 12:35:01 +0100

    checkstyle fixxed, .classpath , .project , .checktsyle worden meegesynct

--- SS/src/ss/week3/pw/StrongChecker.java
+++ SS/src/ss/week3/pw/StrongChecker.java
@@ -1,19 +1,14 @@
 package ss.week3.pw;
 
-public class StrongChecker implements Checker {
+public class StrongChecker extends BasicChecker implements Checker {
 	
+	@Override
 	public boolean acceptable(String suggestion) {
-		
-		if (!(suggestion.contains(" ") || suggestion.length() < 7)) {
+		Character fchar = suggestion.charAt(0);
+		Character lchar = suggestion.charAt(suggestion.length() - 1);
+		return super.acceptable(suggestion) && 
+						(Character.isLetter(fchar) && Character.isDigit(lchar));
 			
-				Character fchar = suggestion.charAt(0);
-				Character lchar = suggestion.charAt(suggestion.length() - 1);
-				return (fchar.isLetter() && isDigit(lchar));
-
-			
-		}
-		else {return false;}
-		
 		
 	}
 
