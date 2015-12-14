591b0afd926b2345b8606e8966bb14880c594676
Sander <s.p.r.bloemen@student.utwente.nl>
2015-12-14 21:22:50 +0100

    week 5 - p5.3

--- SS/src/ss/week5/MapUtil.java
+++ SS/src/ss/week5/MapUtil.java
@@ -1,22 +1,53 @@
 package ss.week5;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapUtil {
-    public static <K, V> boolean isOneOnOne(Map<K, V> map) {
-        // TODO: implement, see exercise P-5.1
-        return false;
+	
+    /*@ ensures (\forall int i, j; 0 < i & i < \old(map.values().size()) 
+					& 0 < j & j < \old(map.values().size()) & i != j;
+					 !\old(map.values().toArray()[i]).equals(\old(map.values().toArray()[j])))
+					 	<==> \result == true; @*/
+	
+	public static <K, V> boolean isOneOnOne(Map<K, V> map) {
+        for (V value : map.values()) {
+        	ArrayList<V> lijst = new ArrayList<V>(map.values());
+        	lijst.remove(value);
+        	if (lijst.contains(value)) {
+        		return false;
+        	}
+        }
+        return true;
     }
+	
+	/*@ ensures (\forall int i; 0 < i & i < \old(range.size());
+	 				map.containsValue(range.toArray()[i]) <==> \result == true);@*/
     public static <K, V> 
            boolean isSurjectiveOnRange(Map<K, V> map, Set<V> range) {
-        // TODO: implement, see exercise P-5.2
-        return false;
+        for (V value : range) {
+        	if (!map.containsValue(value)) {
+        		return false;
+        	}
+        }
+        return true;
     }
+    
+    
     public static <K, V> Map<V, Set<K>> inverse(Map<K, V> map) {
-        // TODO: implement, see exercise P-5.3
+    	Map<V, Set<K>> result = new HashMap<V, Set<K>>();
+        for (K key : map.keySet()) {
+        	V value = map.get(key);
+        	if (!result.containsKey(value)) {
+        		HashSet<K> keyset = new HashSet<K>();
+            	keyset.add(key);
+            	result.put(value, keyset);
+        	}
+        	result.
+        }
         return null;
 	}
 	public static <K, V> Map<V, K> inverseBijection(Map<K, V> map) {
